12381f038e817d820030e206a28bcb56fb37217a
Tianlu Yuan <tianlutheman@gmail.com>
2016-11-11 17:25:16 -0600

    Added stringify function

--- snowflake/library.py
+++ snowflake/library.py
@@ -3,6 +3,11 @@
 from icecube import icetray
 from icecube.icetray import I3Units
 from icecube import astro
+from collections import namedtuple, defaultdict
+import numpy as np
+
+# namedtuple class for storing charges and times for each dom
+DomInfo = namedtuple('DomInfo', 'dom times charges')
 
 def excluded_doms(frame, exclude_list, keep_partial=True):
     """Returns a list of excluded doms for the current frame based on the
@@ -44,4 +49,31 @@ def print_event(particle, header):
     print 'mjd = {:.2f}'.format(mjd)
     print 'dec, ra = {:.2f}, {:.2f}'.format(eqtr.dec/I3Units.deg,
                                             eqtr.ra/I3Units.deg)
-    
+
+
+def stringify(all_pulses, min_q=0):
+    """sorts pulses into a dictionary with string numbers as keys and the
+    doms on that string with their pulses as values
+
+    all_pulses: an I3RecoPulseSeriesMap
+    min_q: minimum total charge requirement
+
+    returns: {i3string:[Pulse1,...]}
+    """
+    i3strings = defaultdict(list)
+    # loop over all pulses on doms across all strings
+    for dom in all_pulses.iterkeys():
+        pulses = all_pulses[dom]
+        times, charges = [], []
+        for pulse in pulses:
+            times.append(pulse.time)
+            charges.append(pulse.charge)
+        times = np.array(times)
+        charges = np.array(charges)
+        # append each dom's pulse onto string
+        if np.sum(charges) > min_q:
+            i3strings[dom.string].append(DomInfo(dom,
+                                                 times,
+                                                 charges))
+
+    return i3strings
