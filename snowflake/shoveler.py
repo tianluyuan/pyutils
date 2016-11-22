f5c1742555d2b0d8a0d0ce5689b9ae6f9180b3f3
Tianlu Yuan <tianlutheman@gmail.com>
2016-11-22 14:10:20 -0600

    added snowflake/shoveler to reshape cascades

---
+++ snowflake/shoveler.py
@@ -0,0 +1,12 @@
+""" Useful functions for working with steamshovel
+"""
+from I3Tray import I3Units
+from icecube import dataclasses
+
+def cascade_reshape(cascade):
+    """ A cascade will by default be rendered as a sphere. This gives it a direction and speed.
+    """
+    cascade.shape = dataclasses.I3Particle.InfiniteTrack
+    cascade.type = dataclasses.I3Particle.MuMinus
+    cascade.speed = 3.*10**8 * I3Units.m/I3Units.s
+    cascade.length = 0.1 * I3Units.m
