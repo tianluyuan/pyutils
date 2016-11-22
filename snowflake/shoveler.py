e72a39e6cf1e0864b41ae79be51dc6882d56b459
Tianlu Yuan <tianlutheman@gmail.com>
2016-11-22 14:45:21 -0600

    changed particle.type in cascade_reshape to I3Particle.unknown insteadof muminus

--- snowflake/shoveler.py
+++ snowflake/shoveler.py
@@ -7,6 +7,6 @@ def cascade_reshape(cascade):
     """ A cascade will by default be rendered as a sphere. This gives it a direction and speed.
     """
     cascade.shape = dataclasses.I3Particle.InfiniteTrack
-    cascade.type = dataclasses.I3Particle.MuMinus
+    cascade.type = dataclasses.I3Particle.unknown
     cascade.speed = 3.*10**8 * I3Units.m/I3Units.s
     cascade.length = 0.1 * I3Units.m
