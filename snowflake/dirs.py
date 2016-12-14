fccd7c515b5395ffc0f11aa68dc206295e24c825
Tianlu Yuan <tianlutheman@gmail.com>
2016-12-14 12:33:44 -0600

    add DATA to dirs

--- snowflake/dirs.py
+++ snowflake/dirs.py
@@ -4,3 +4,4 @@ import os
 
 FIG = os.path.join('/home', os.getenv('USER'), 'public_html', 'share', 'fig')
 STORE = os.path.join('/data', 'user', os.getenv('USER'), 'store')
+DATA = os.path.join('/home', os.getenv('USER'), 'projects', 'icecube', 'data')
