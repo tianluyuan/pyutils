f33b5c924938aff41e39ae7d08ecf0f8e368f1a9
Tianlu Yuan <tianlutheman@gmail.com>
2017-04-01 11:25:06 -0500

    add function colorlines to plot lines with changing col

--- common/plotter.py
+++ common/plotter.py
@@ -1,4 +1,5 @@
 from matplotlib.backends.backend_pdf import PdfPages
+from matplotlib import colors
 import matplotlib.pyplot as plt
 
 
@@ -11,3 +12,16 @@ def multipage(filename, figs=None, dpi=200):
     for fig in figs:
         fig.savefig(pp, format='pdf')
     pp.close()
+
+
+def colorlines(x, y, ncolors=5, cmapname='copper_r', **kwargs):
+    """ Plot a line plot in which the lines change colors
+    """
+    cmap = plt.get_cmap(cmapname)
+    norm = colors.Normalize(vmin=0, vmax=ncolors-1)
+    for i in range(ncolors):
+        chunksize = len(x)/ncolors
+        low = i*chunksize
+        # add 1 to keep lines connected
+        high = min((i+1)*chunksize+1, len(x))
+        plt.plot(x[low:high], y[low:high], color=cmap(norm(i)))
