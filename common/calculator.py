a571465b17de976c2e57b24272717c5d5cde5d84
Tianlu Yuan <tianlutheman@gmail.com>
2017-04-03 14:13:21 -0500

    add edges function to calculator

--- common/calculator.py
+++ common/calculator.py
@@ -5,6 +5,13 @@ def centers(x):
     return (x[:-1]+x[1:])*0.5
 
 
+def edges(x):
+    """ returns bin edges with centers that match x. approx "inverse" of center(x).
+    """
+    c = centers(x)
+    return np.concatenate(([2*x[0]-c[0]], c, [2*x[-1]-c[-1]]))
+
+
 def calc_nbins(x):
     n =  (np.max(x) - np.min(x)) / (2 * len(x)**(-1/3) * (np.percentile(x, 75) - np.percentile(x, 25)))
     return np.floor(n)
