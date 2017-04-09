207fb9383eda7340ffb4c69dc112007544c8fe99
Tianlu Yuan <tianlutheman@gmail.com>
2017-04-08 21:38:20 -0500

    add hese_names function

--- snowflake/library.py
+++ snowflake/library.py
@@ -1,8 +1,7 @@
 """ Useful functions for working with icecube software
 """
-from I3Tray import I3Tray
-from icecube import icetray
-from icecube.icetray import I3Units
+from I3Tray import I3Tray, I3Units
+from icecube import icetray, dataclasses
 from icecube import astro
 from icecube.hdfwriter import I3HDFWriter
 from collections import namedtuple, defaultdict
@@ -121,3 +120,50 @@ def hdfwriter(inp, out, subeventstreams=None, keys=None, types=None):
              types=types)
     tray.Execute()
     tray.Finish()
+
+
+def hese_names(run_id, event_id=None):
+    """ stolen from ckopper's file. this is probably incomplete.
+
+    *run_id* can be either run_id or an I3EventHeader
+    *event_id* needs to be specified if header is a run_id
+    """
+    if isinstance(run_id, dataclasses.I3EventHeader):
+        event_id = run_id.event_id
+        run_id = run_id.run_id
+
+    names = {
+        (118178,66452255):'Camilla the Chicken',	#3
+        (118283,9445773):'Rowlf',	#5
+        (118381,19162840):'Beaker',	#7
+        (118435,58198553):'Mr. Snuffleupagus',	#9
+        (118545,63733662):'Bert',	#11
+        (118549,11722208):'Sweetums',	#13
+        (118602,23096391):'Dr. Bunsen Honeydew',	#15
+        (118607,40435683):'Lew Zealand',	#17
+        (119196,37713300):'Zoot',	#19
+        (119214,8606380):'Link Hogthrob',	#21
+        (119316,36556705):'Ernie',	#23
+        (119352,56498321):'Guy Smiley',	#25
+        (119404,80750561):'Miss Piggy',	#27
+        (119470,48424887):'Gonzo the Great',	#29
+        (119474,33152537):'Rizzo',	#31
+        (119595,30769232):'Count von Count',	#33
+        (119674,8449256):'Kermit',	#35
+        (119842,82622124):'Sam Eagle',	#37
+        (120045,22615214):'Dr. Teeth',	#39
+        (115994,2538090):'Fozzy',	#41
+        (115994,29874216):'Scooter',	#43
+        (116528,52433389):'Animal',	#45
+        (116698,10198436):'Swedish Chef',	#47
+        (116876,63208734):'Dr. Strangepork',	#49
+        (116878,34919596):'Statler and Waldorf',	#51
+        (117322,7422546):'Pepe the King Prawn',	#53
+        (117371,31623515):'Crazy Harry',	#55
+        (117782,49441871):'Floyd',	#57
+        (118145,5142726):'Oscar'	#59
+    }
+    try:
+        return names[(run_id, event_id)]
+    except KeyError:
+        return '{}, {}'.format(run_id, event_id)
