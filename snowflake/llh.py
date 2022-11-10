python engine seems to fix pandas ParserError buffer overflow

snowflake/llh.py
@@ -58,7 +58,7 @@ def read_single(llhout, llhcut=np.inf, lpat=r'^[+0-9]'):
 
     llhdat = pd.read_csv(llhout, delim_whitespace=True, header=None,
                          names='l rlogl x y z zenith azimuth e t a b'.split(),
-                          usecols=lambda x: x[0] in 'lrxyzaetb')
+                         usecols=lambda x: x[0] in 'lrxyzaetb', engine='python')
     select = llhdat['l'].str.match(lpat)
     llhdat = llhdat.loc[select].apply(pd.to_numeric, errors='ignore')
     lssdat = pd.read_csv(llhout, delimiter=':', header=None,