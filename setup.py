c5573e25a9790ea8c8fda22f45d0f2bae05f717c
Tianlu Yuan <5412915+tianluyuan@users.noreply.github.com>
2023-06-21 11:53:47 -0500

    add workflows for pypi

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-setup(name='pyutils',
+setup(name='pyutilz',
       version='0.0.1',
       description='A package with useful python tools',
       url='https://github.com/tianluyuan/pyutils',
