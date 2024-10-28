a92f483fff63f1308b7ead95fccc9b13d4ab0434
Tianlu Yuan <5412915+tianluyuan@users.noreply.github.com>
2024-10-28 11:50:13 -0500

    bump patchver

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='pyutils_hep',
-      version='0.1.4',
+      version='0.1.5',
       description='A package with useful python tools',
       long_description=open('README.md').read(),
       long_description_content_type='text/markdown',
